6701f5c881747dd6c2044c842f6e0c98f7df35cd
Phillips <gp079799@c02zv456md6r.attlocal.net>
2020-08-29 01:54:10 -0500

    Add vscode keybind file

--- scripts/install-osx.sh
+++ scripts/install-osx.sh
@@ -3,10 +3,11 @@
 BASE_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd )"
 
 # Remove old dotfiles or links
-rm -rf ~/.zsh_aliases ~/.zsh_prompt ~/.zshrc ~/Library/Application\ Support/Code/User/settings.json
+rm -rf ~/.zsh_aliases ~/.zsh_prompt ~/.zshrc ~/Library/Application\ Support/Code/User/settings.json ~/Library/Application\ Support/Code/User/keybindings.json
 
 # Link to dotfiles
 ln -sf ${BASE_DIR}/zsh/.zsh_aliases ~/.zsh_aliases
 ln -sf ${BASE_DIR}/zsh/.zsh_prompt ~/.zsh_prompt
 ln -sf ${BASE_DIR}/zsh/.zshrc ~/.zshrc
 ln -sf ${BASE_DIR}/vscode/settings.json ~/Library/Application\ Support/Code/User/settings.json
+ln -sf ${BASE_DIR}/vscode/keybindings.json ~/Library/Application\ Support/Code/User/keybindings.json
