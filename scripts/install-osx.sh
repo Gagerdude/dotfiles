0e2c0123ccb16985df6f8ae0f1f0bb81a7996ba7
Phillips <gp079799@c02zv456md6r.attlocal.net>
2020-08-28 23:27:25 -0500

    add vscode settings

--- scripts/install-osx.sh
+++ scripts/install-osx.sh
@@ -3,8 +3,9 @@
 BASE_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd )"
 
 # Remove old dotfiles or links
-rm -rf ~/.zsh_aliases ~/.zshrc
+rm -rf ~/.zsh_aliases ~/.zshrc ~/Library/Application\ Support/Code/User/settings.json
 
 # Link to dotfiles
 ln -sf ${BASE_DIR}/zsh/.zsh_aliases ~/.zsh_aliases
 ln -sf ${BASE_DIR}/zsh/.zshrc ~/.zshrc
+ln -sf ${BASE_DIR}/vscode/settings.json ~/Library/Application\ Support/Code/User/settings.json
